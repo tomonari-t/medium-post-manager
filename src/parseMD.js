1fe0a72753b4def0c164139a4d97cccd106e72d0
tomonari_t <tomo1208.japan@gmail.com>
2018-04-12 08:11:03 +0900

    WIP

--- src/parseMD.js
+++ src/parseMD.js
@@ -1,17 +1,23 @@
+const isMDList = text => /-\s\S+/.test(text);
+
 const parseMD = (mdString) => {
   const mdLines = mdString.trim().split('\n');
   const tags = [];
-  for (let index = 0; index < mdLines.length; index += 1) {
-    const text = mdLines[index];
-    if (/-\s\S+/.test(text)) {
+  mdLines.forEach((text, index) => {
+    let isBreak = false;
+    if (!isMDList(text)) {
+      isBreak = true;
+    }
+
+    if (!isBreak) {
       mdLines.splice(index, 1);
       tags.push(text.trim());
     }
+  });
   const parsedVal = {
     tags,
     content: mdLines.join('\n'),
   };
-
   return parsedVal;
 };
 
